e8d870ae459d5ff9220224529d543173312094a2
Junaxer <juniordelarocha15@hotmail.com>
2025-06-19 18:56:57 -0600

    Modificando main (#8)

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+def mostrar_menu():
+    print("=== Menú Principal ===")
+    print("1. Saludar")
+    print("2. Salir")
 
 def main():
-    print("Hola")
-
-main()
+    while True:
+        mostrar_menu()
+        opcion = input("Elige una opción: ")
+        if opcion == "1":
+            print("Hola")
+        elif opcion == "2":
+            print("¡Hasta luego!")
+            break
+        else:
+            print("Opción no válida. Intenta de nuevo.")
